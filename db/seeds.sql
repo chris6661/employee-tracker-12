d15468426fde6dc277969c5d5a41678d671223b1
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-03-10 18:11:09 -0600

    added names to employee section in seeds sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -22,4 +22,15 @@ VALUES
 
 INSERT INTO employees (first_name, last_name, role_id, mgr_id)
         VALUES
-            ('')
+    ("Nick", "Fury", 1, null),
+    ("Maria", "Hill", 2, 1),
+    ("Phil", "Coulson", 2, 1),
+    ("Steve", "Rogers", 3, null),
+    ("Natasha", "Romanoff", 4, 4),
+    ("Yo-Yo", "Rodriguez", 4, 4),
+    ("Cint", "Barton", 5, 4),
+    ("Jimmy", "Woo", 6, null),
+    ("Peggy", "Carter", 7, null),
+    ("Howard", "Stark", 8, 9),
+    ("Hank", "Pym", 9, null),
+    ("Sharon", "Carter", 10, 11);
