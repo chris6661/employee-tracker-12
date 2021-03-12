7a7130d7ea5d1fc7a0a4e140ca4204f4b0521469
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-03-11 22:23:28 -0600

    added create functions for employee, department, and role

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-var x = 15; 
-var y = 20;
-print(x+y);
+const connection = require('./connection'); 
+
+//create functions
+function createDepartment(dpt_name) {
+    const sql = `INSERT INTO departments SET dpt_name = ?`;
+    const params = [dpt_name];
+    return connection.promise().query(sql, params)
+    .then(([rows, fields]) => console.log(`\n${dpt_name} department added. \n`))
+    .catch(err => console.log(err)); 
+}
+
+function createRole(newRole) {
+    const sql = `INSERT INTO roles (title, salary, dpt_id) VALUES (?, ?, ?)`; 
+    return connection.promise().query(sql, newRole)
+    .then(([rows, fields]) => console.log(`\nNew job title '${newRole[0]}' added. \n`))
+    .catch(err => console.log(err)); 
+}
+
+function createEmployee(newEmployee) {
+    const sql = `INSERT INTO employees (first_name, last_name, role_id, mgr_id) VALUES (?, ?, ?, ?)`;
+    return connection.promise().query(sql, newEmployee)
+    .then(([rows, fields]) => console.log(`\n${newEmployee[0]} ${newEmployee[1]} was added to the databaase. \n`))
+    .catch(err => console.log(err)); 
+}
+
+//read functions    
+function readAllDepartments() {
+    const sql = 
+}
