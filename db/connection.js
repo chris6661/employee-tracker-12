8680beeaeac85526f6fd1aa146eb9c48c04d08e4
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-03-11 22:57:21 -0600

    added connection info in js file

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3000, 
+    user: 'root', 
+    password: 'Gates7dSme!i90', 
+    database: 'tracker_db'
+}); 
+
+connection.connect(err => {
+    if (err) throw err; 
+    console.log('\nConnected as id' + connection.threadId); 
+}); 
+
+module.exports = connection; 
